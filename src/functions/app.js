2054ddcbf9e7e8157552538659a69e8ae681f799
Lorenzo Caballero <zeldiuss@gmail.com>
2025-03-31 14:45:21 -0300

    messege

--- src/functions/app.js
+++ src/functions/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from 'body-parser';
-import { empleadosRoutes } from "../routes/empleados.routes.js";
+import  empleadosRoutes  from "../routes/empleados.routes.js";
 
 const app = express();
 
